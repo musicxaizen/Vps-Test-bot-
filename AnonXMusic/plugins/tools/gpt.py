d67adb248297bacf4d6de1a173474256d31436ca
Soulaizen77 <hritikraj123412312@gmail.com>
2024-11-09 12:03:22 +0530

    Update and rename gpt (1).py to gpt.py

--- AnonXMusic/plugins/tools/gpt.py
+++ AnonXMusic/plugins/tools/gpt.py
@@ -24,13 +24,13 @@ def ensure_english(text):
         print(f"Error detecting or translating text: {e}")
     return text
 
-@app.on_message(filters.command(["izen"], prefixes=["a", "A"]))
+@app.on_message(filters.command(["chigo"], prefixes=["i", "I"]))
 async def chat_arvis(app, message):
     try:
         await app.send_chat_action(message.chat.id, ChatAction.TYPING)
         name = message.from_user.first_name
         if len(message.command) < 2:
-            await message.reply_text(f"ʜᴇʏ ᴍʏ sᴏɴ {name}  , Wʜʏ ᴀʀᴇ ʏᴏᴜ ᴄᴀʟʟɪɴɢ ᴍᴇ 😒")
+            await message.reply_text(f"ʜᴇʏ {name}  , Wʜʏ ᴀʀᴇ ʏᴏᴜ ᴄᴀʟʟɪɴɢ ᴍᴇ ")
         else:
             query = message.text.split(' ', 1)[1]
             MODEL = "gpt-3.5-turbo"
@@ -51,7 +51,7 @@ async def chat_gpt(app, message):
         await app.send_chat_action(message.chat.id, ChatAction.TYPING)
 
         if len(message.command) < 2:
-            await message.reply_text("Hᴇʏ {name} Nɪɢɢᴀ, I ᴀᴍ 𝐀ɪᴢᴇɴ . Wʜᴀᴛ ᴅᴏ ʏᴏᴜ ᴡᴀɴɴᴀ ᴋɴᴏᴡ ? 😒")
+            await message.reply_text("Hᴇʏ , I ᴀᴍ  . I ᴀᴍ ʜᴇʀᴇ ᴛᴏ ᴀssɪsᴛ ʏᴏᴜ! Tᴇʟʟ ᴍᴇ ᴡʜᴀᴛ ᴅᴏ ʏᴏᴜ ᴡᴀɴɴᴀ ᴋɴᴏᴡ ")
         else:
             query = message.text.split(' ', 1)[1]
             MODEL = "gpt-3.5-turbo"
@@ -72,7 +72,7 @@ async def chat_annie(app, message):
         await app.send_chat_action(message.chat.id, ChatAction.TYPING)
         name = message.from_user.first_name
         if len(message.command) < 2:
-            await message.reply_text(f"Hello {name}, I am AIZEN . How can I help you today?")
+            await message.reply_text(f"Hello {name}, I am ICHIGO . How can I help you today?")
         else:
             query = message.text.split(' ', 1)[1]
             MODEL = "gpt-3.5-turbo"
