85b5a3968cda2eacd64f044244d7038d7ea6eab1
flexOfficial20 <155304501+flexOfficial20@users.noreply.github.com>
2024-11-10 00:11:27 +0530

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/AnonymousX1025/AnonXMusic",
+    "https://github.com/musicxaizen/Vps-Test-bot-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
